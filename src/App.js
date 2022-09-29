fd6b5a86a6979a6065784e1b8f081c2c7aba7c73
Tahmina <tahminakhatun5447@gmail.com>
2022-09-29 06:36:49 +0600

    add icon

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCoffee } from '@fortawesome/free-solid-svg-icons'
 import AllCards from './Components/AllCards/AllCards';
 import Questions from './Components/Questions/Questions';
 import Sidebar from './Components/Sidebar/Sidebar';
@@ -16,7 +18,7 @@ function App() {
   
   return (
     <div className='px-10'>
-      <h1 className='text-4xl text-red-700 font-bold text-center py-10'>Cook For Yourself</h1>
+      <h1 className='text-4xl text-red-700 font-bold text-center py-10'> <FontAwesomeIcon className='mr-4' icon={faCoffee} />Cook For Yourself</h1>
       <div className='flex'>
         <div className='w-3/5 text-center '>
           <AllCards handleAddBtn={handleAddBtn}></AllCards>
